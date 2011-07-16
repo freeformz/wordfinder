fc4b3166a3fccc32654af9be9ec37b7f82fcc9b8
freeformz <edwardam@interlix.com>
2011-07-15 22:38:23 -0700

    shake

--- tests/test_wordfinder.rb
+++ tests/test_wordfinder.rb
@@ -14,25 +14,25 @@ class WordFinderTest < MiniTest::Unit::TestCase
   def test_it_says_words
     get '/'
     assert last_response.ok?
-    assert_equal last_response.body, 'Words!'
+    assert_equal "Words!\n", last_response.body
   end
 
   def test_it_returns_500
     post '/find'
     assert last_response.status == 500
-    assert_equal last_response.body, 'Letters Missing'
+    assert_equal 'Letters Missing', last_response.body
   end
 
   def test_it_finds_words
     post '/find', :letters => 'ame'
     assert last_response.ok?
-    assert_equal last_response.body, "[\"AE\",\"AM\",\"EA\",\"ME\",\"MA\",\"EM\",\"MAE\"]"
+    assert_equal "[\"AE\",\"AM\",\"EA\",\"ME\",\"MA\",\"EM\",\"MAE\"]", last_response.body
   end
 
   def test_finding_and_length
     post '/find', :letters => 'ame', :length => 2
     assert last_response.ok?
-    assert_equal last_response.body, "[\"AE\",\"AM\",\"EA\",\"EM\",\"MA\",\"ME\"]", last_response.body.inspect
+    assert_equal "[\"AE\",\"AM\",\"EA\",\"EM\",\"MA\",\"ME\"]", last_response.body, last_response.body.inspect
   end
 
 end
