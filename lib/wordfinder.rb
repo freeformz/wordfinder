e0e0e7bd5fb07537ace7c4627a206ac78741c18a
freeformz <edwardam@interlix.com>
2011-07-15 21:32:56 -0700

    small refactor

--- lib/wordfinder.rb
+++ lib/wordfinder.rb
@@ -4,18 +4,28 @@ require 'json'
 
 WORDS = File.open("#{File.dirname(__FILE__)}/../words/sowpods.txt",'r') { |f| f.read.split("\r\n") }
 
-def find_words_for(search_letters)
-  WORDS.select do |word| 
-    letters = search_letters.clone
-    word.chars.all? do |char|
-      if pos = letters.index(char)
-        letters.delete_at(pos)
-        true
-      else
-        false
+# Finds words
+#
+# @param [Array] search_letters An array of letters to search
+# @param [Fixnum] max_word_length the maximum word length to return
+#
+# Brute Force FTL
+def find_words(words, search_letters, max_word_length = 8)
+  words.select do |word| 
+    if word.length > max_word_length
+      false
+    else
+      letters = search_letters.clone
+      word.chars.all? do |char|
+        if pos = letters.index(char)
+          letters.delete_at(pos)
+          true
+        else
+          false
+        end
       end
     end
-  end.flatten.compact.sort {|a,b| a.length <=> b.length }.select {|w| w.length <= 8 }
+  end.flatten.compact.sort {|a,b| a.length <=> b.length }
 end
 
 get '/' do
@@ -26,5 +36,5 @@ post "/find" do
   unless letters = params[:letters]
     halt 500, "Letters Missing"
   end
-  find_words_for(letters.chars.map(&:upcase)).to_json
+  find_words(WORDS, letters.chars.map(&:upcase)).to_json
 end
